8a7607e0026f39b552fbc5e7596c163a8d6ba26c
Brent-Morrison <brentjohnmorrison@hotmail.com>
2025-09-09 08:37:30 +1000

    Working app

--- app.R
+++ app.R
@@ -1,3 +1,5 @@
+# https://shiny.posit.co/r/gallery/application-layout/retirement-simulation/
+
 library(shiny)
 library(dplyr)
 library(tidyr)
@@ -13,28 +15,55 @@ source("./R/f.R")
 # Read data ----------------------------------------------------------------------------------------------------------------
 
 d <- get_data()
-dat_df          <- d$dat
-chart_df        <- d$chart 
-txn_df          <- d$txn_type
-cx_df           <- d$cx_delta 
-ox_df           <- d$ox_delta
+dat_df    <- d$dat
+chart_df  <- d$chart 
+txn_df    <- d$txn_type
+cx_df     <- d$cx_delta 
+ox_df     <- d$ox_delta
 
+
+
+# Function to specify UI inputs for rendering
 renderInputs <- function(prefix) {
   wellPanel(
     fluidRow(
-      column(6,
-             sliderInput(paste0(prefix, "_", "cost_of_debt_nmnl"), "Nominal cost of debt :", min = 0.01, max = 0.10, step = 0.01 , value = 0.04),
-             sliderInput(paste0(prefix, "_", "fcast_infltn")     , "Forecast inflation :"  , min = 0.01, max = 0.07, step = 0.005, value = 0.025)
+      column(
+        6,
+        sliderInput(
+          inputId = paste0(prefix, "_", "cost_of_debt_nmnl"), 
+          #inputId = "cost_of_debt_nmnl", 
+          label   = "Nominal cost of debt :", 
+          min     = 0.01, 
+          max     = 0.10, 
+          step    = 0.01, 
+          value   = 0.04
+          ),
+        sliderInput(
+          inputId = paste0(prefix, "_", "fcast_infltn"),
+          #inputId = "fcast_infltn", 
+          label   = "Forecast inflation :", 
+          min     = 0.01, 
+          max     = 0.07, 
+          step    = 0.005, 
+          value   = 0.025
+          )
       ),
-      column(6,
-             selectInput(paste0(prefix, "_", "oxcx_scenario")    , "Scenario:", choices = c("scnr1","scnr2","scnr3","scnr4"))
+      column(
+        6,
+        selectInput(
+          inputId = paste0(prefix, "_", "oxcx_scenario"), 
+          #inputId = "oxcx_scenario", 
+          label   = "Scenario:", 
+          choices = c("scnr1","scnr2","scnr3","scnr4")
+          )
       )
     ),
-    p(actionButton(paste0(prefix, "_", "recalc"),
-                   "Re-run simulation", icon("random")
-    ))
+    p(actionButton(inputId = "recalc", label = "Re-run simulation", icon = icon("random"))
+    )
   )
 }
+
+
 
 # Define UI for application that plots random distributions
 ui <- fluidPage(
@@ -57,60 +86,51 @@ ui <- fluidPage(
     column(6, tags$h3("Scenario A")),
     column(6, tags$h3("Scenario B"))
   ),
+  
+  # Renders the data input sliders
   fluidRow(
     column(6, renderInputs("a")),
     column(6, renderInputs("b"))
   ),
+  
+  # Renders the plots 
   fluidRow(
-    column(6, plotOutput("a_distPlot", height = "600px")),
-    column(6, plotOutput("b_distPlot", height = "600px"))
+    column(6, plotOutput("a_plot", height = "600px")),
+    column(6, plotOutput("b_plot", height = "600px"))
   )
 )
 
 
 
-
-
-
-
-paramNames <- c("dat","chart","txn_type","cx_delta","ox_delta","cost_of_debt_nmnl", "fcast_infltn", "oxcx_scenario")
-
+# Server function
 server <- function(input, output, session) {
   
-  getParams <- function(prefix) {
-    input[[paste0(prefix, "_recalc")]]
-    
-    params <- lapply(paramNames, function(p) {
-      input[[paste0(prefix, "_", p)]]
-    })
-    names(params) <- paramNames
-    params
-  }
+  simA <- reactive(list(
+    f( dat=dat_df, chart=chart_df, txn_type=txn_df, cx_delta=cx_df, ox_delta=ox_df,  
+       q_grow            = 0.19,
+       cost_of_debt_nmnl = input$a_cost_of_debt_nmnl, 
+       fcast_infltn      = input$a_fcast_infltn,
+       roe               = 0.041, 
+       debt_sens         = NULL, 
+       oxcx_scenario     = input$a_oxcx_scenario,
+       verbose           = F))
+    )
+  simB <- reactive(list(
+    f( dat=dat_df, chart=chart_df, txn_type=txn_df, cx_delta=cx_df, ox_delta=ox_df,  
+       q_grow            = 0.19,
+       cost_of_debt_nmnl = input$b_cost_of_debt_nmnl, 
+       fcast_infltn      = input$b_fcast_infltn,
+       roe               = 0.041, 
+       debt_sens         = NULL, 
+       oxcx_scenario     = input$b_oxcx_scenario,
+       verbose           = F))
+    )
   
-  # Function that generates scenarios and computes NAV. The expression
-  # is wrapped in a call to reactive to indicate that:
-  #
-  #  1) It is "reactive" and therefore should be automatically
-  #     re-executed when inputs change
-  #
-  simA <- reactive(do.call(f, getParams("a")))
-  simB <- reactive(do.call(f, getParams("b")))
-  
-  # Expression that plot NAV paths. The expression
-  # is wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should be automatically
-  #     re-executed when inputs change
-  #  2) Its output type is a plot
-  #
-  output$a_distPlot <- renderPlot({
-    plot_kpi(simA())
-  })
-  output$b_distPlot <- renderPlot({
-    plot_kpi(simB())
-  })
+  output$a_plot <- renderPlot( {plot_kpi( simA() )} )
+  output$b_plot <- renderPlot( {plot_kpi( simB() )} )
   
 }
+
 
 
 # app.R
